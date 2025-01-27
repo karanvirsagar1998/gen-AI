3dc5212233d558560fae6370a9bf4b4b9163292b
Karanvir Singh <karanvirsagar1998@gmail.com>
2025-01-27 18:56:52 +0000

    h

--- resume-matcher.py
+++ resume-matcher.py
@@ -16,7 +16,7 @@ import PyPDF2
 
 nlp = spacy.load("en_core_web_sm")
 
-def get_text_from_pdf_resume(pdf_path):
+def get_text_from_pdf_file(pdf_path):
     with open(pdf_path, 'rb') as pdf: #Opens the specified pdf_file in binary read mode ('rb').
         reader = PyPDF2.PdfReader(pdf) #Creates an instance of the PdfReader class from the PyPDF2 library, passing the opened file as an argument.
         text=""
@@ -24,7 +24,7 @@ def get_text_from_pdf_resume(pdf_path):
             text+=page.extract_text()
         return text
 
-def get_text_from_word_resume(word_path):
+def get_text_from_word_file(word_path):
     doc = Document(word_path) #Document class from the python-docx library to load the .docx file.
     full_text = []
     for para in doc.paragraphs:
@@ -36,60 +36,48 @@ def extact_email(doc):
     emails = re.findall(email_pattern, doc)
     return emails if emails else None
 
+def extract_education(doc):
+    doc = nlp(doc)
+    #some of common requirements for everyjob
+    education = ["bachelor", "master", "phd", "associate", "diploma", "degree", "b.tech", 
+    "bachelor's", "master's", "mba", "mca", "msc", "bsc", "m.tech"]
+    found_education = set()
+    for token in doc:
+        if token.text.lower() in education:
+            found_education.add(token.text.lower())
+    return found_education
+
 def extract_skills(doc):
+    doc = nlp(doc)
+    #pre-defined skill for a job requirement...
     skills = ["maintenance", "coordination", "supervision","reporting","management", "Fire alarm handling", "after-hours support"]
-    found_skills = [skill for skill in skills if skill.lower() in doc.lower()]
+    found_skills = set() #creating a set to avoid same skills twice
+    for token in doc:
+        if token.text.lower() in skills:
+            found_skills.add(token.text.lower())
     return found_skills
 
-def extract_we(doc):
-    we = []
+def extract_workExperience(doc):
+    workExperience = []
     experience_pattern = r"(?:[A-Za-z]+(?: [A-Za-z]+)*)\s*(\d{4}[-/]\d{4}|\d{4})" 
     experience = re.findall(experience_pattern, doc)
     for exp in experience:
-        we.append(exp)
-    return we
-
-def analyze_resume(resume_text):
-    doc = nlp(resume_text) # Process the text using SpaCy
-    # named_entities = [(ent.text, ent.label_) for ent in doc.ents]
-    # email = extact_email(resume_text)
-    skills = extract_skills(resume_text)
-    # we = extract_we(resume_text)
-    # return{
-    #     "Named Entities": named_entities,
-    #     "Skills": skills,
-    #     "Work Experience": we,
-    #     "Email" : email            
-    # }
-    return skills
-
-resume_text = get_text_from_pdf_resume("resumes/document.pdf") #get text from resume
+        workExperience.append(exp)
+    return workExperience
 
 def text_prepprocess(resume_text): #ensures that both the resume and job description are cleaned, standardized, and ready for comparison.
     text = resume_text.lower() #Convert text to lowercase
     text = re.sub(r'[^a-z\s]', '', text) #Remove special characters, numbers, and punctuation
     # r'[^a-z\s]': Matches anything that isn’t a lowercase letter or space.
     # re.sub(): Replaces matched characters with an empty string ('').
-    #Tokenize and lemmatize using SpaCy
-    doc = nlp(text)
+    doc = nlp(text)  #Tokenize and lemmatize using SpaCy
     tokens = [token.lemma_ for token in doc if not token.is_stop]
     return " ".join(tokens) #Join the processed tokens back into a single string
 
-# resume_analysis = analyze_resume(resume_text)
 
-jd = get_text_from_pdf_resume("resumes/jd.pdf")
-job_desc_processed = text_prepprocess(jd)
-resume_processed = text_prepprocess(resume_text)
+from sklearn.feature_extraction.text import CountVectorizer # Converts text into a numerical format (word frequency vectors).
+from sklearn.metrics.pairwise import cosine_similarity # Measures how similar two numerical vectors are.
 
-# Extract important keywords (like skills, tools, and roles) from both the resume and job description.
-# Use set operations or NLP techniques to find overlapping keywords.
-# Calculate a similarity score based on the match.
-
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-
-# CountVectorizer: Converts text into a numerical format (word frequency vectors).
-# cosine_similarity: Measures how similar two numerical vectors are.
 
 def calculate_similarity(resume, job_description): # Accepts two preprocessed strings: the resume and job description.
     texts = [resume, job_description]  # Combines them into a list (texts), so both can be vectorized together.
@@ -101,21 +89,26 @@ def calculate_similarity(resume, job_description): # Accepts two preprocessed st
 
 def calculate_detailed_score(resume, job_description):
     resume_sections = {
-        "skills":", ".join(analyze_resume(resume)),
+        "skills":", ".join(extract_skills(resume)),
+        "education": ", ".join(extract_education(resume)),
     }
     job_description = {
-        "skills": "Oversee all on-site construction,Fire Alarm, after-hours,  management"+
-                    " including maintenance, coordinating, and supervision of subcontractors, vendors, and laborers."
+        #we will use hard-code for now, dynamic for improvisation..
+        "skills": "Oversee all on-site construction, Fire Alarm, after-hours, management including maintenance, coordinating, and supervision of subcontractors, vendors, and laborers.",
+        "education": "bachelor, master, phd, associate, diploma, degree, btech, bachelor's, master's, mba, mca, msc, bsc, mtech",
     }
-
     scores = {}
     for category in resume_sections:
         scores[category] = calculate_similarity(resume_sections[category], job_description[category])
     return scores
 
-
-
 #Testing....
+resume_text = get_text_from_word_file("resumes/Resume.docx") #get text from resume
+jd = get_text_from_word_file("resumes/JD.docx") #get text from jd
+
+job_desc_processed = text_prepprocess(jd)
+resume_processed = text_prepprocess(resume_text)
+
 scores = calculate_detailed_score(resume_processed, job_desc_processed)
 for category, score in scores.items():
     print(f"{category.capitalize()} Match: {score}%")
